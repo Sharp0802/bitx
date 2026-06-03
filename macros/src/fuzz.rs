de7b8d65b16187d3e04d9a4ba19349bedf2c1976
sharp0802 <sharp0802.business@gmail.com>
2026-06-03 18:10:41 +0900

    :test: rewrite fuzz harness using new APIs

--- macros/src/fuzz.rs
+++ macros/src/fuzz.rs
@@ -1,19 +1,28 @@
 #![allow(missing_docs)]
 #![no_main]
 
-mod data;
-mod field;
-mod lit;
-mod off;
-mod variant;
+mod ast;
+mod cg;
+mod hir;
+mod prelude;
+mod tt;
 
-use data::Data;
 use libfuzzer_sys::fuzz_target;
 use proc_macro2::TokenStream;
-use syn::parse2;
+use quote::ToTokens;
 
 fuzz_target!(|data: &str| {
     if let Ok(tokens) = data.parse::<TokenStream>() {
-        let _ = parse2::<Data>(tokens);
+        let mut input: tt::Input = tokens.into();
+
+        let Ok(ast) = input.parse::<ast::Data>() else {
+            return;
+        };
+
+        let Ok(hir) = TryInto::<hir::Data>::try_into(ast) else {
+            return;
+        };
+
+        _ = hir.to_token_stream();
     }
 });
