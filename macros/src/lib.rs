1a5f1d3f56b4509784e4d889ab6c9827a9e54129
sharp0802 <sharp0802.business@gmail.com>
2026-06-03 17:52:21 +0900

    :feat: expose new syntaxes

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,24 +3,26 @@
 mod ast;
 mod cg;
 mod hir;
-mod lit;
-mod off;
 mod prelude;
+mod tt;
 
-use prelude::*;
-use proc_macro as pm;
-
-fn bits_impl(ast: ast::Data) -> Result<pm::TokenStream> {
-    let _hir: hir::Data = ast.try_into()?;
-    //ast.to_token_stream().into()
-    Ok(pm::TokenStream::new())
-}
+use proc_macro::TokenStream;
+use quote::ToTokens;
 
 #[proc_macro]
-pub fn bits(input: pm::TokenStream) -> pm::TokenStream {
-    let ast = syn::parse_macro_input!(input as ast::Data);
-    match bits_impl(ast) {
-        Ok(output) => output,
-        Err(err) => err.into_compile_error().into(),
-    }
+pub fn bits(input: TokenStream) -> TokenStream {
+    let input: proc_macro2::TokenStream = input.into();
+    let mut input: tt::Input = input.into();
+
+    let ast: ast::Data = match input.parse() {
+        Ok(ast) => ast,
+        Err(err) => return err.to_token_stream().into(),
+    };
+
+    let hir: hir::Data = match ast.try_into() {
+        Ok(hir) => hir,
+        Err(err) => return err.to_token_stream().into(),
+    };
+
+    hir.to_token_stream().into()
 }
