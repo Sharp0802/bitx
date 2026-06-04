1382b3ad4f8eacece6418dbb215af4482a614833
sharp0802 <sharp0802.business@gmail.com>
2026-06-04 18:18:26 +0900

    :chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -21,18 +21,34 @@
         };
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            openssl
-            pkg-config
-            llvmPackages.libcxx
-            llvmPackages.clangUseLLVM
-            rust-bin.nightly.latest.default
-          ];
+        devShells.default =
+          pkgs.mkShell.override
+            {
+              stdenv = pkgs.llvmPackages.libcxxStdenv;
+            }
+            {
+              buildInputs = with pkgs; [
+                openssl
+                pkg-config
+                llvmPackages.clangUseLLVM
+                (rust-bin.nightly.latest.default.override {
+                  extensions = [ "llvm-tools-preview" ];
+                })
+                cargo-fuzz
+                cargo-tarpaulin
+                cargo-llvm-cov
+              ];
 
-          CXXSTDLIB = "c++";
-          RUSTFLAGS = "-C linker=clang++ -C link-arg=-lc++";
-        };
+              CC = "clang";
+              CXX = "clang++";
+              TARGET_CC = "clang";
+              TARGET_CXX = "clang++";
+              HOST_CC = "clang";
+              HOST_CXX = "clang++";
+              CXXFLAGS = "-stdlib=libc++";
+              CXXSTDLIB = "c++";
+              RUSTFLAGS = "-C linker=clang++ -C link-arg=-lc++";
+            };
       }
     );
 }
